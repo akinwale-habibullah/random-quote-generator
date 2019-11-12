8b0aa3022ec3f89e08569de0b600c6229a4c281e
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-12 17:01:43 +0100

    generate random quote

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import QuoteComponent from './components/QuoteComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: {
+        quote: 'Hello World',
+        author: 'Akinwale Habib'
+      }
+    }
+
+    this.makeHttpRequest = this.makeHttpRequest.bind(this);
+    this.generateNewQuote = this.generateNewQuote.bind(this);
+  }
+
+  async makeHttpRequest(endpoint){
+    let quote;
+    try {
+      quote = await axios.get(endpoint);
+    } catch (error) {
+      throw new Error('HTTP request failed. Please try again later');
+    }
+    return quote.data.quotes[0];
+  }
+
+  async generateNewQuote(e){
+    e.preventDefault();
+    const endpoint = 'https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=true';
+    const quote = await this.makeHttpRequest(endpoint);
+    this.setState((state, props) => ({ quote }))
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <QuoteComponent quote={this.state.quote} generateQuote={this.generateNewQuote}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/QuoteComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './QuoteComponent.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
+const QuoteComponent = (props) => {
+  return (
+    <div>
+      <div className="quote-div">
+        <p id="quote-string">
+          <i className="fas fa-quote-left"></i>  
+          {props.quote.quote}
+          <br />
+          <br />
+          <span id="author">
+            - {props.quote.author ? props.quote.author : 'Unknown'}
+          </span>
+        </p>
+      </div>
+      <div className="controls-div">
+        <button 
+          id="new-quote"
+          onClick={props.generateQuote}>
+          New Quote
+        </button>
+        <div id="share">
+          <a
+            href="https://google.com"><i className="fab fa-facebook-f"></i></a>
+          <a
+            href="https://twitter.com"><i className="fab fa-twitter"></i></a>
+        </div>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default QuoteComponent;
