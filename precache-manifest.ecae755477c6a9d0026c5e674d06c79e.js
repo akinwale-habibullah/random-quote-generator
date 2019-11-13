492def4ba95de8c953ec61720bf00f9bee4610e3
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-13 09:35:52 +0100

    Updates

---
+++ precache-manifest.ecae755477c6a9d0026c5e674d06c79e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0857cbc6218ee915459b6feaff3b3e8e",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "7dc5d6c1dbf78c4cb2c4",
+    "url": "/random-quote-generator/static/css/2.aa6f5a05.chunk.css"
+  },
+  {
+    "revision": "ba2514f65f1aa62d6e59",
+    "url": "/random-quote-generator/static/css/main.41d5bb18.chunk.css"
+  },
+  {
+    "revision": "7dc5d6c1dbf78c4cb2c4",
+    "url": "/random-quote-generator/static/js/2.b2f27460.chunk.js"
+  },
+  {
+    "revision": "ba2514f65f1aa62d6e59",
+    "url": "/random-quote-generator/static/js/main.c3370ff3.chunk.js"
+  },
+  {
+    "revision": "67fdbfa62430d219b82c",
+    "url": "/random-quote-generator/static/js/runtime-main.602dca97.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/random-quote-generator/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/random-quote-generator/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/random-quote-generator/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/random-quote-generator/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/random-quote-generator/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/random-quote-generator/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/random-quote-generator/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/random-quote-generator/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/random-quote-generator/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/random-quote-generator/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/random-quote-generator/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/random-quote-generator/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/random-quote-generator/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/random-quote-generator/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/random-quote-generator/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
